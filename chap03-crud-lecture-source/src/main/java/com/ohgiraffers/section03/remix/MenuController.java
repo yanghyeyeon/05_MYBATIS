90dbf207487b1798f373539bcc41d96b4ea1e2f1
yanghyeyeon <yhy4058@gmail.com>
2024-06-20 13:18:27 +0900

    remix menucontroller

---
+++ chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section03/remix/MenuController.java
@@ -0,0 +1,98 @@
+package com.ohgiraffers.section03.remix;
+
+
+import java.util.List;
+import java.util.Map;
+
+public class MenuController {
+
+    private final PrintResult printResult;
+    private final MenuService menuService;
+
+    public MenuController() {
+
+        // 생성자 주입
+        printResult = new PrintResult();
+        menuService = new MenuService();
+    }
+
+    // 전체 조회
+    public void selectAllMenu() {
+
+        List<MenuDTO> menuList = menuService.selectAllMenu();
+
+        if (menuList != null) {
+            printResult.printMenuList(menuList);
+        } else {
+            printResult.printErrorMessage("selectList");
+        }
+    }
+
+    // 메뉴코드로 단건 조회
+    public void selectMenuByCode(Map<String, String> parameter) {
+
+        int code = Integer.parseInt(parameter.get("code"));
+
+        MenuDTO menu = menuService.selectMenuByCode(code);
+
+        if (menu != null) {
+            printResult.printMenu(menu);
+        } else {
+            printResult.printErrorMessage("selectOne");
+        }
+    }
+
+    // 신규 메뉴 등록
+    public void registMenu(Map<String, String> parameter) {
+
+        String name = parameter.get("name");
+        int price = Integer.parseInt(parameter.get("price"));
+        int categoryCode = Integer.parseInt(parameter.get("categoryCode"));
+
+        MenuDTO menu = new MenuDTO();
+
+        menu.setName(name);
+        menu.setPrice(price);
+        menu.setCategoryCode(categoryCode);
+
+        if (menuService.registMenu(menu)) {
+            printResult.printSuccessMessage("insert");
+        } else {
+            printResult.printErrorMessage("insert");
+        }
+    }
+
+    // 메뉴 수정
+    public void modifyMenu(Map<String, String> parameter) {
+
+        int code = Integer.parseInt(parameter.get("code"));
+        String name = parameter.get("name");
+        int price = Integer.parseInt(parameter.get("price"));
+        int categoryCode = Integer.parseInt(parameter.get("categoryCode"));
+
+        MenuDTO menu = new MenuDTO();
+
+        menu.setCode(code);
+        menu.setName(name);
+        menu.setPrice(price);
+        menu.setCategoryCode(categoryCode);
+
+        if (menuService.modifyMenu(menu)) {
+            printResult.printSuccessMessage("update");
+        } else {
+            printResult.printErrorMessage("update");
+        }
+    }
+
+    // 메뉴 삭제
+    public void deleteMenu(Map<String, String> parameter) {
+
+        int code = Integer.parseInt(parameter.get("code"));
+
+        if (menuService.deleteMenu(code)) {
+            printResult.printSuccessMessage("delete");
+        } else {
+            printResult.printErrorMessage("delete");
+        }
+    }
+}
