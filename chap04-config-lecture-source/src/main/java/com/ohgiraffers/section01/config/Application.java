e538009349aba282faa49073fbe69b4621659ca9
yanghyeyeon <yhy4058@gmail.com>
2024-06-23 10:58:37 +0900

    chap04 config

---
+++ chap04-config-lecture-source/src/main/java/com/ohgiraffers/section01/config/Application.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.section01.config;
+
+import com.ohgiraffers.common.Template;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println(Template.getSqlSession());
+    }
+}
