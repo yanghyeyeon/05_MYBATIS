e430b49b9fdb4d243316dab4cd86ed8ed16a5015
yanghyeyeon <yhy4058@gmail.com>
2024-06-24 11:22:16 +0900

    java mapper elementtestservice

--- chap06-mapper-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestService.java
+++ chap06-mapper-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestService.java
@@ -1,4 +1,53 @@
 package com.ohgiraffers.section01.xmlmapper;
 
+import com.ohgiraffers.common.MenuDTO;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+import static com.ohgiraffers.common.Template.getSqlSession;
+
 public class ElementTestService {
+
+    private ElementTestMapper mapper;
+
+    public void selectCacheTest() {
+
+        SqlSession sqlSession = getSqlSession();
+
+        mapper = sqlSession.getMapper(ElementTestMapper.class);
+
+        for(int i = 0; i < 10; i++) {
+
+            // 조회시간 확인 용 시작 시간
+            Long startTime = System.currentTimeMillis();
+
+            List<String> nameList = mapper.selectCacheTest();
+            System.out.println(nameList);
+
+            // 조회시간 확인 용 종료 시간
+            Long endTime = System.currentTimeMillis();
+
+            Long interval = endTime - startTime;
+            System.out.println("수행시간 : " + interval + "(ms)");
+
+        }
+
+        sqlSession.close();
+    }
+
+    public void selectResultMapTest() {
+
+        SqlSession sqlSession = getSqlSession();
+
+        mapper = sqlSession.getMapper(ElementTestMapper.class);
+
+        List<MenuDTO> menuList = mapper.selectResultMapTest();
+
+        for(MenuDTO menu : menuList) {
+            System.out.println(menu);
+        }
+
+        sqlSession.close();
+    }
 }
