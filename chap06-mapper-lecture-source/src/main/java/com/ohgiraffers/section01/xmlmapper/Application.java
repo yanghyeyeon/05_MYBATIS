b0b8affe3fb2f54c2336d2bee1ea177ae1da64c9
yanghyeyeon <yhy4058@gmail.com>
2024-06-24 11:20:40 +0900

    mapper application

--- chap06-mapper-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/Application.java
+++ chap06-mapper-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/Application.java
@@ -20,13 +20,38 @@ public class Application {
             int no = sc.nextInt();
 
             switch (no) {
-                case 1: elementTestService.selectCacheTest(); break;
-                case 2: break;
+                case 1:
+                    elementTestService.selectCacheTest(); break;
+                case 2:
+                    resultMapSubMenu(); break;
                 case 3: break;
                 case 4: break;
 
             }
         }while (true);
+    }
+
+    private static void resultMapSubMenu() {
+
+        Scanner sc = new Scanner(System.in);
+        ElementTestService elementTestService = new ElementTestService();
+
+        do {
+            System.out.println("================= <resultMap> 서브 메뉴 =================");
+            System.out.println("1. <resultMap> 테스트");
+            System.out.println("2. <constructor> 테스트");
+            System.out.println("1. <association> 테스트");
+            System.out.println("1. <collection> 테스트");
+            System.out.print("메뉴 번호를 입력하세요 : ");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1: elementTestService.selectResultMapTest(); break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+            }
+        } while (true);
     }
 
 }
