403e20a05258396eeb050815e54b8112b8a8c656
yanghyeyeon <yhy4058@gmail.com>
2024-06-24 12:15:34 +0900

    menuandcategorydto 추가

--- chap06-mapper-lecture-source/src/main/java/com/ohgiraffers/common/MenuAndCategoryDTO.java
+++ chap06-mapper-lecture-source/src/main/java/com/ohgiraffers/common/MenuAndCategoryDTO.java
@@ -1,2 +1,18 @@
-package com.ohgiraffers.common;public class MenuAndCategoryDTO {
+package com.ohgiraffers.common;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class MenuAndCategoryDTO {
+
+    private int code;
+    private String name;
+    private int price;
+    private CategoryDTO category;
+    private String orderableStatus;
 }
