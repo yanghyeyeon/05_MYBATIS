154eeb0aff137774328e5311bb330dcc849df798
yanghyeyeon <yhy4058@gmail.com>
2024-06-24 11:21:38 +0900

    java mapper elementtestmapper

--- chap06-mapper-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestMapper.java
+++ chap06-mapper-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestMapper.java
@@ -1,7 +1,11 @@
 package com.ohgiraffers.section01.xmlmapper;
 
+import com.ohgiraffers.common.MenuDTO;
+
 import java.util.List;
 
 public interface ElementTestMapper {
     List<String> selectCacheTest();
+
+    List<MenuDTO> selectResultMapTest();
 }
