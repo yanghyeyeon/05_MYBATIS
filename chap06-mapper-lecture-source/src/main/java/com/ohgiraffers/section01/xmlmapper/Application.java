9113ab00cff257fb2a6e70d6645f0dc58ad602ba
yanghyeyeon <yhy4058@gmail.com>
2024-06-24 10:00:25 +0900

    mapper

---
+++ chap06-mapper-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/Application.java
@@ -0,0 +1,32 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ElementTestService elementTestService = new ElementTestService();
+
+        do {
+            System.out.println("================ 매퍼 element 테스트 메뉴 ================");
+            System.out.println("1. <cache> 테스트");
+            System.out.println("2. <resultMap> 서브메뉴");
+            System.out.println("3. <sql> 테스트");
+            System.out.println("4. <insert> 서브메뉴");
+            System.out.print("메뉴 번호를 입력하세요 : ");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+
+            }
+        }while (true);
+    }
+
+}
---
+++ chap06-mapper-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestService.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+public class ElementTestService {
+}
